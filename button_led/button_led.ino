bd98d2cdff36aec49d4f0c1518baf35584558072
Ben Van Treese <vantreeseba@gmail.com>
2014-07-13 23:10:13 -0500

    got shift-register reading from high end of parallel, need to change to 16 bit on read, got button with pulldown working to turn on led

---
+++ button_led/button_led.ino
@@ -0,0 +1,15 @@
+int buttonPin = 7;
+int ledPin = 13;
+int buttonPressed = 0;
+
+void setup() {
+  pinMode(buttonPin, INPUT);
+  pinMode(ledPin, OUTPUT);  
+}
+
+
+void loop() {
+  delay(10);        // delay in between reads for stability
+  buttonPressed = digitalRead(buttonPin);
+  digitalWrite(ledPin, buttonPressed);
+}
---
+++ shift_register/shift_register.ino
@@ -0,0 +1,71 @@
+int loadPin = 6;
+int clockPin = 7;
+int dataPin = 8;
+int ledPin = 13;
+
+int bitOrder = MSBFIRST;
+byte value = 0;
+
+void setup() { 
+  pinMode(loadPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  pinMode(dataPin, INPUT);
+  pinMode(ledPin, OUTPUT); 
+  
+  digitalWrite(clockPin, HIGH);
+  Serial.begin(9600);
+}
+
+void loop() {
+  loadData(loadPin, clockPin);
+  value = readData(dataPin, clockPin);
+  
+
+  if(value & 1){
+    Serial.println("button pushed");
+  }
+  
+  if(value & 2){
+    Serial.println("switch on");
+  }
+  
+  if(value == 0){
+    Serial.println("nothing on");
+  }
+    
+
+  delay(1);        // delay in between reads for stability
+}
+
+void loadData(int loadPin, int clockPin){
+  digitalWrite(clockPin, HIGH);
+  digitalWrite(loadPin,HIGH);
+  delayMicroseconds(1);
+  digitalWrite(clockPin, LOW);
+  digitalWrite(loadPin, LOW);
+  delayMicroseconds(1);
+  digitalWrite(clockPin, HIGH);
+  delayMicroseconds(5);
+}
+
+byte readData(int dataPin, int clockPin){
+  byte data = 0;
+  long bitVal = 0;
+  
+  for(int i = 0; i < 8; i++){
+    bitVal = digitalRead(dataPin);
+
+        /* Set the corresponding bit in bytesVal.
+        */
+        data |= (bitVal << ((8-1) - i));
+
+        /* Pulse the Clock (rising edge shifts the next bit).
+        */
+        digitalWrite(clockPin, HIGH);
+        delayMicroseconds(5);
+        digitalWrite(clockPin, LOW);
+  }
+  
+  return data;
+}
+
